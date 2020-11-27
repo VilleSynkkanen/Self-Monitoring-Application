c826143fd7605cdff47524d85a5f834cd17707cc
Ville Synkkänen <ville.synkkanen@aalto.fi>
2020-11-27 16:28:30 +0200

    Structure. middlewares, etc.

---
+++ src/config/config.js
@@ -0,0 +1,15 @@
+let config = {};
+
+if (Deno.env.get('TEST_ENVIRONMENT')) {
+  config.database = {};
+} else {
+  config.database = {
+    hostname: "hostname-possibly-at-elephantsql.com",
+    database: "database-name",
+    user: "user-name-typically-same-as-database-name",
+    password: "password",
+    port: 5432
+  };
+}
+
+export { config }; 
