fe8fa0d545894686ca60bf8fb698681eb24fe9e0
Ville Synkkänen <ville.synkkanen@aalto.fi>
2020-12-06 16:40:19 +0200

    Logging middleware

--- src/middleware/middlewares.js
+++ src/middleware/middlewares.js
@@ -20,11 +20,31 @@ const authenticationMiddleware = async({request, response, session}, next) => {
   }
 }
 
-const requestTimingMiddleware = async({ request }, next) => {
+const requestTimingMiddleware = async({ request, session }, next) => {
+  const today = new Date();
+  let auth = "anonymous";
+  if (session && await session.get('authenticated')) 
+  {
+    auth = (await session.get('user')).id
+  }
+
+  let hours = today.getHours();
+  if(hours <= 9)
+    hours = "0" + hours;
+
+  let minutes = today.getMinutes();
+  if(minutes <= 9)
+    minutes = "0" + minutes;
+  
+  let seconds = today.getSeconds();
+  if(seconds <= 9)
+    seconds = "0" + seconds;
+  
+  const time = hours + ":" + minutes + ":" + seconds;
   const start = Date.now();
   await next();
   const ms = Date.now() - start;
-  console.log(`${request.method} ${request.url.pathname} - ${ms} ms`);
+  console.log(`${time} ${request.method} ${request.url.pathname} - ${ms} ms ID: ${auth}`);
 }
 
 const serveStaticFilesMiddleware = async(context, next) => {
