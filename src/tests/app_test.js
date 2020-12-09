6cd97e8845121aec0b66a1b3b9677f06b1fdb1d0
Ville Synkkänen <ville.synkkanen@aalto.fi>
2020-12-09 11:44:13 +0200

    Testing goals

--- src/tests/app_test.js
+++ src/tests/app_test.js
@@ -1,22 +1,22 @@
 /* 
-*  1.
-*  2.
-*  3.
-*  4.
-*  5.
-*  6.
-*  7.
-*  8.
-*  9.
-*  10.
-*  11.
-*  12.
-*  13.
-*  14. 
-*  15.
-*  16.
-*  17.
-*  18.
-*  19.
-*  20.
-*/
+*  1. SummaryAPI 1
+*  2. SummaryAPI 2
+*  3. ReportAPI 1 success
+*  4. ReportAPI 1 fail
+*  5. ReportAPI 2 success
+*  6. ReportAPI 2 fail
+*  7. Registration
+*  8. => Login
+*  9. Registration fail
+*  10. Login fail
+*  11. Logout
+*  12. Auth middleware
+*  13. Error middleware
+*  14. Logging middleware
+*  15. EJS page
+*  16. EJS page
+*  17. EJS page
+*  18. EJS page
+*  19. EJS page
+*  20. EJS page
+*/ 
